69aa3b236a86d7ff750004048dc789ec54b73dae
Roman Kornilov <r.kornilov@lar.tech>
2021-04-13 18:32:02 +0300

    progress

--- rawData_on_csv_log_2021.py
+++ rawData_on_csv_log_2021.py
@@ -15,6 +15,8 @@ import csv
 import pylab# модуль построения поверхности
 from mpl_toolkits.mplot3d import Axes3D# модуль построения поверхности
 from scipy.signal import butter, lfilter, freqz
+import statistics
+
 
 FD = 3200
 G_SCALE_FACTOR = 0.004
@@ -24,23 +26,42 @@ USE_POINTS = 2*4096
 START_POINT_INDEX = 0
 
 DEAD_ZONE =2*G_SCALE_FACTOR
-CALC_START_POS = 13
+CALC_START_POS = 1
 deadzone_graph =[]
 
 py_velocity = .0
 
-velosity_histoty =[]
+velocity_histoty =[]
+velocity_fft =[]
 
 def butter_lowpass(cutoff, fs, order=5):
     nyq = 0.5 * fs
     normal_cutoff = cutoff / nyq
     b, a = butter(order, normal_cutoff, btype='low', analog=False)
+    print('b', b, sep = '   ')
+    print('a', a, sep = '   ')
     return b, a
 
 def butter_lowpass_filter(data, cutoff, fs, order=5):
     b, a = butter_lowpass(cutoff, fs, order=order)
     y = lfilter(b, a, data)
     return y
+    
+def butter_bandpass(lowcut, highcut, fs, order=5):
+    nyq = 0.5 * fs
+    low = lowcut / nyq
+    high = highcut / nyq
+    b, a = butter(order, [low, high], btype='band')
+    print('b', b, sep = '   ')
+    print('a', a, sep = '   ')
+    return b, a
+
+
+def butter_bandpass_filter(data, lowcut, highcut, fs, order=5):
+    b, a = butter_bandpass(lowcut, highcut, fs, order=order)
+    y = lfilter(b, a, data)
+    return y
+
     
 def toFixed(numObj, digits=0):
     return f"{numObj:.{digits}f}"
@@ -84,7 +105,7 @@ def plot_signal_fft_and_history(adxl_signal, freqs, history, velocity, python_fr
     plt.plot(adxl_signal)
     plt.subplot(312)
     plt.plot(python_freqs[START_POS:], python_fft[START_POS:], label=label_name)
-    plt.plot(python_freqs[START_POS:], deadzone_graph[START_POS:], color='green', label=velocity + " mm/s")
+    plt.plot(python_freqs[START_POS:], deadzone_graph[START_POS:], color='green', label=velocity + " mm/s,   DEAD_ZONE = " + str(toFixed(DEAD_ZONE,5)))
     plt.legend(loc='best')
     plt.subplot(313)
     plt.plot(freqs[START_POS:], history[START_POS:], label=velocity)
@@ -95,46 +116,62 @@ def plot_fft_and_history(adxl_signal, freqs, history, velocity, python_freqs, py
     START_POS = 1
     plt.subplot(211)
     plt.plot(python_freqs[START_POS:], python_fft[START_POS:], label=label_name)
-    plt.plot(python_freqs[START_POS:], deadzone_graph[START_POS:], color='green', label=velocity + " mm/s")
+    plt.plot(python_freqs[START_POS:], deadzone_graph[START_POS:], color='green', label=velocity + " mm/s,   DEAD_ZONE = " + str(toFixed(DEAD_ZONE,5)))
     plt.legend(loc='best')
     plt.subplot(212)
     plt.plot(freqs[START_POS:], history[START_POS:], label=velocity)
     plt.legend(loc='best')
     plt.show()
 
+def plot_signal_fft_and_fftvelocity(python_freqs, adxl_signal, python_fft, fft_velocity, velocity, label_name):
+    START_POS = 1
+    plt.subplot(311)
+    plt.plot(adxl_signal)
+    plt.subplot(312)
+    plt.plot(python_freqs[START_POS:], python_fft[START_POS:], label=label_name)
+    plt.plot(python_freqs[START_POS:], deadzone_graph[START_POS:], color='green', label=velocity + " mm/s,   DEAD_ZONE = " + str(toFixed(DEAD_ZONE,5)))
+    plt.legend(loc='best')
+    plt.subplot(313)
+    plt.plot(python_freqs[START_POS:], fft_velocity[START_POS:], label=velocity)
+    plt.legend(loc='best')
+    plt.show()
+
 
 def calc_python_velocity(freq_array, fft_array):
     global py_velocity
-    global velosity_histoty
+    global velocity_histoty
+    global velocity_fft
     #print('python velocity')
     velocity = 0
     count = 0
     for i in range(CALC_START_POS, len(freq_array)):
-    #for i in range(CALC_START_POS, 1280):
-    #for i in range(CALC_START_POS, 512):
-        if fft_array[i] > DEAD_ZONE:
-            velocity += pow(fft_array[i] / freq_array[i], 2)
+        if (freq_array[i] > 9.5) & (freq_array[i] < 1000) & (fft_array[i] > DEAD_ZONE) :
+            velocity_fft[i] =  (fft_array[i]/pow(2, 0.5)) * G_MM_S2 * MM_IN_METER / (2*pi * freq_array[i]) #перевод из амплитуды в дискретах ускорения в СКЗ виброскорости на этой частоте
             count += 1
-            print("[", i, "] ", toFixed(freq_array[i],2), " Hz   ", toFixed(fft_array[i],5), " g   ", toFixed(pow(velocity * pow( G_MM_S2 * MM_IN_METER, 2) / (4 * pow(pi, 2)), 0.5) / pow(2, 0.5),3), " mm/s", sep='')
-        velosity_histoty[i] =  velocity
+            velocity += pow( velocity_fft[i], 2)
+            print("[", i, "] ", toFixed(freq_array[i],2), " Hz   ", toFixed(fft_array[i],5), " g   ", toFixed(velocity_fft[i],3), " mm/s  ", toFixed(pow(velocity,0.5),3) , " mm/s", sep='')
+        velocity_histoty[i] =  pow(velocity,0.5)
 
-    velocity = pow(velocity * pow( G_MM_S2 * MM_IN_METER, 2) / (4 * pow(pi, 2)), 0.5) / pow(2, 0.5)
-
-    for i in range(CALC_START_POS, len(freq_array)):
-        velosity_histoty[i] = pow(velosity_histoty[i]* pow( G_MM_S2 * MM_IN_METER, 2) / (4 * pow(pi, 2)), 0.5) / pow(2, 0.5)
-
-    #print('velocity_xyz_points', count,sep='   ')
+    velocity = pow(velocity,0.5)
     print('velocity', velocity, sep='   ')
-
     py_velocity = velocity
 
 
 
 def save_in_point_to_c_file(input_points):
     f_out = open("in_array.c", "w")
-    f_out.write("int16_t z_in_array[4096][2] = {\r")
+    f_out.write("const int16_t z_in_array[" + str(len(input_points)) +"][2] = {\r")
     for i in range(len(input_points)):
         f_out.write('{ ' + str(int(input_points[i][0])) + ' , ' + str(int(input_points[i][1])) + ' },\r')
+    f_out.write("};\r")
+    f_out.close()
+
+
+def save_signal_to_c_file(signal):
+    f_out = open("in_array.c", "w")
+    f_out.write("const int16_t z_in_array[" + str(len(signal)) +"][2] = {\r")
+    for i in range(len(signal)):
+        f_out.write('{ ' + str(i) + ' , ' + str(int(signal[i])) + ' },\r')
     f_out.write("};\r")
     f_out.close()
 
@@ -220,10 +257,80 @@ tune_coeff = 10000
 FD = 3200
 
 
+
+
+input_file = "2021.04.05\\AB4-LT_7.94 _566Гц_10.csv"
+input_file = "2021.04.05\\AB4-LT F88A5EA2A577_9.91_10Гц_10.csv"
+input_file = "2021.04.05\\AB4-LT F88A5EA2A577_10.14_40Гц_10.csv"
+input_file = "2021.04.05\\AB4-LT F88A5EA2A577_0.42_8Гц_10.csv"
+input_file = "2021.04.05\\AB4-LT F88A5EA2A577_1.76_25Гц_2.csv"
+input_file = "2021.04.05\\AB4-LT F88A5EA2A577_3.87_10Гц_4.csv"
+tune_coeff = 10140 #1577
+FD = 3200
+
+
+
+input_file = "2021.04.07\\AB4-LT F88A5EA2A9E5_2.62.csv"
+input_file = "2021.04.07\\AB4-LT F88A5EA2A9E5_4.92.csv"
+input_file = "2021.04.07\\AB4-LT F88A5EA2A9E5_2.02.csv"
+tune_coeff = 9970 #A9E5
+FD = 3200
+
+input_file = "2021.04.07\\AB4-LT F88A5EA2ABA5_2.78.csv"
+input_file = "2021.04.07\\AB4-LT F88A5EA2ABA5_4.15.csv"
+input_file = "2021.04.07\\AB4-LT F88A5EA2ABA5_13.4.csv"
+tune_coeff = 10300 #BA5
+FD = 3200
+
+
+
+input_file = "2021.04.08\\AB4-LT F88A5EA2A7F7_2.41.csv"
+input_file = "2021.04.08\\AB4-LT F88A5EA2A7F7_0.44.csv"
+input_file = "2021.04.08\\AB4-LT F88A5EA2A7F7_0.csv"
+input_file = "2021.04.08\\AB4-LT F88A5EA2A7F7_7.67.csv"
+input_file = "2021.04.08\\AB4-LT F88A5EA2A7F7_0.12.csv"
+input_file = "2021.04.08\\AB4-LT F88A5EA2A7F7_0.13.csv"
+input_file = "2021.04.08\\AB4-LT F88A5EA2A7F7_1.36.csv"
+input_file = "2021.04.08\\AB4-LT F88A5EA2A7F7_1.14.csv"
+tune_coeff = 10300 #BA5
+FD = 3200
+
+
+
+#Северная ТЭЦ
+#Двигатель №1
+input_file = "2021.04.13\\AB4-LT F88A5EA2ABA5_11.57.csv"
+input_file = "2021.04.13\\AB4-LT F88A5EA2ABA5_11.56.csv"
+tune_coeff = 10300 #BA5
+FD = 3200
+
+#Двигатель №1
+input_file = "2021.04.13\\AB4-LT F88A5EA2A9E5_7.52.csv"
+input_file = "2021.04.13\\AB4-LT F88A5EA2A9E5_7.84.csv"
+tune_coeff = 9970 #A9E5
+FD = 3200
+
+#Двигатель №2
+input_file = "2021.04.13\\AB4-LT F88A5EA2A9E5_2.79.csv"
+input_file = "2021.04.13\\AB4-LT F88A5EA2A9E5_2.78.csv"
+tune_coeff = 9970 #A9E5
+FD = 3200
+
+#Двигатель №2
+input_file = "2021.04.13\\AB4-LT F88A5EA2ABA5_4.72.csv"
+input_file = "2021.04.13\\AB4-LT F88A5EA2ABA5_1.31.csv"
+input_file = "2021.04.13\\AB4-LT F88A5EA2ABA5_1.73.csv"
+tune_coeff = 10300 #BA5
+FD = 3200
+
+
+
+
 # подставить сюда нужный файл
 # или см. дальше, где "модель сигнала вместо данных из файла"
-input_file = "2021.04.02\\AB4-LT F88A5EA2ABA5_0.84.csv"
-tune_coeff = 10300
+
+input_file = "2021.04.05\\AB4-LT F88A5EA2A577_10.14_40Гц_10.csv"
+tune_coeff = 10140 #1577
 FD = 3200
 
 
@@ -243,42 +350,59 @@ for row in reader:
    
 
 #модель сигнала вместо данных из файла
-if 1 :
-    USE_POINTS = 4*2048   
+if 0 :
+    #USE_POINTS = 4*2048   
     FD = 3200
     CALC_START_POS = int(10*USE_POINTS/FD+1)
     input_points = []
-    input_signal_hz = 9
+    input_signal_hz = 0.09
     file_descr = " model " + str(input_signal_hz) +" Hz [" + str(USE_POINTS) + " points " + str(FD) + " Hz]"
     for i in range(USE_POINTS):
-        model_signal = 1/G_SCALE_FACTOR*sin(input_signal_hz*i/FD*2*pi)
-        #model_signal += 0.5/G_SCALE_FACTOR*sin(100*i/FD*2*pi)
+        model_signal = 1/G_SCALE_FACTOR*sin(input_signal_hz*i/FD*2*pi+pi/2)
+        #model_signal += 1/G_SCALE_FACTOR*sin(900*i/FD*2*pi)
+        #if i > G_SCALE_FACTOR/20 : model_signal += 1/G_SCALE_FACTOR
         #model_signal += 0.08/G_SCALE_FACTOR*sin(77*i/FD*2*pi)
         input_points.append([float(i), float(model_signal)])
 
 
 
    
-#print(input_points)
-save_in_point_to_c_file(input_points)
-
 
 n=[input_points[i][0] for i in range(len(input_points))]
 signal = [input_points[i][1] for i in range(len(input_points))]
+
 #window = np.hamming(len(signal))
-window = np.hanning(len(signal))
-signal = signal*window
+#window = np.hanning(len(signal))
+#signal = signal*window
 
 
 # Filter requirements.
 order = 6
 fs = 3200.0       # sample rate, Hz
-cutoff = 600      # desired cutoff frequency of the filter, Hz
+lowcut = 50.0
+highcut = 800.0
 # Get the filter coefficients so we can check its frequency response. # b, a = butter_lowpass(cutoff, fs, order)
+level = 2000
+if 1 :
+    signal -= np.mean(signal)
+    for i in range(len(signal)):
+        if signal[i] > level :
+            signal[i] = level
+        if signal[i] < -level :
+            signal[i] = -level
 
-#signal = butter_lowpass_filter(signal, cutoff, fs, order)
+if 0 :
+    signal -= np.mean(signal)
+    signal = butter_lowpass_filter(signal, highcut, fs, order)
+    
 
-#print(signal)
+if 0 :
+    signal -= np.mean(signal)
+    signal = butter_bandpass_filter(signal, lowcut, highcut, fs, order)
+    
+
+save_signal_to_c_file(signal)
+
 
 spectrum = rfft(signal)
 furie_amplitudes = np_abs(spectrum)
@@ -293,16 +417,17 @@ for i in range(len(furie_norm_amplitudes)) :
     furie_freqs.append( i*(((FD*10000)/tune_coeff)/2)/((len(furie_norm_amplitudes)) - 1) )
 
 for i in range(len(furie_freqs)) :
-    velosity_histoty.append(0)
+    velocity_histoty.append(0)
+    velocity_fft.append(0)
+
+#DEAD_ZONE = statistics.median(furie_norm_amplitudes[3328:3840]) # 1300..1500 Hz (4096/3200)*freq  
+DEAD_ZONE = 1*G_SCALE_FACTOR
+
+print(file_descr)
 
 calc_python_velocity(furie_freqs, furie_norm_amplitudes)
+print("DEAD_ZONE", DEAD_ZONE, sep='   ')
 
-#fig = plt.figure()
-#ax = fig.gca(projection='3d')
-#ax.plot(n,signal,0,label='accel z')
-#ax.legend(loc='best')
-#ax.grid(True)
-#plt.show()
 
 py_velocity = toFixed(py_velocity,3)
 
@@ -312,50 +437,9 @@ for i in range(len(furie_freqs)):
     else: 
         deadzone_graph.append(float(DEAD_ZONE))
 
-plot_signal_fft_and_history(signal, furie_freqs, velosity_histoty, py_velocity, furie_freqs, furie_norm_amplitudes,  str(len(signal)) + " points from " + str(START_POINT_INDEX) + "  in  " + file_descr )
-#plot_fft_and_history(signal, furie_freqs, velosity_histoty, py_velocity, furie_freqs, furie_norm_amplitudes,  str(len(signal)) + " points from " + str(START_POINT_INDEX) + "  in  " + file_descr )
+#plot_signal_fft_and_history(signal, furie_freqs, velocity_histoty, py_velocity, furie_freqs, furie_norm_amplitudes,  str(len(signal)) + " points from " + str(START_POINT_INDEX) + "  in  " + file_descr )
+#plot_fft_and_history(signal, furie_freqs, velocity_histoty, py_velocity, furie_freqs, furie_norm_amplitudes,  str(len(signal)) + " points from " + str(START_POINT_INDEX) + "  in  " + file_descr )
+plot_signal_fft_and_fftvelocity(furie_freqs, signal, furie_norm_amplitudes, velocity_fft, py_velocity, str(len(signal)) + " points from " + str(START_POINT_INDEX) + "  in  " + file_descr )
 
 
 sys.exit()
-
-
-
-WINDOW_LEN = 4096
-windowed_graph = []
-ADDED_LABEL = ""
-for window_shift in range(len(input_points) - WINDOW_LEN) :
-    print('window_shift', window_shift, sep='   ')
-    window_points = []
-    for i in range(WINDOW_LEN):
-        window_points.append(float(input_points[i+window_shift][1]))
-
-    #hann_window = np.hamming(len(window_points))
-    #window_points = window_points*hann_window
-    #ADDED_LABEL = "+ HANN"
-
-    order = 6
-    fs = 3200.0       # sample rate, Hz
-    cutoff = 600      # desired cutoff frequency of the filter, Hz
-    window_points = butter_lowpass_filter(window_points, cutoff, fs, order)
-    ADDED_LABEL = "+ LOWPASS FILTER"
-        
-    spectrum = rfft(window_points)
-    furie_amplitudes = np_abs(spectrum)
-    furie_norm_amplitudes = 2 * G_SCALE_FACTOR * furie_amplitudes / len(window_points)
-    furie_norm_amplitudes[0] /= 2
-    
-    furie_freqs = []
-    for i in range(len(furie_norm_amplitudes)) :
-        furie_freqs.append( i*(((FD*10000)/tune_coeff)/2)/((len(furie_norm_amplitudes)) - 1) )
-        
-    velosity_histoty = []
-    for i in range(len(window_points)) :
-        velosity_histoty.append(0)
-
-    calc_python_velocity(furie_freqs, furie_norm_amplitudes)
-    
-    windowed_graph.append(float(py_velocity))
-
-plt.plot(range(len(input_points) - WINDOW_LEN), windowed_graph, label= input_file + "   window[" + str(WINDOW_LEN) + "]" + ADDED_LABEL)
-plt.legend(loc='best')
-plt.show()
